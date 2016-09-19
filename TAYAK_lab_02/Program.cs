42fad325a055e7ff8d0a97eea4f82c7ce6974b09
GsomGsom <igorsapozhnikov9@gmail.com>
2016-09-19 23:49:23 +0300

    -Добавил выбор для открытия нужного файла-варианта; -Переименовал файл третьего варианта (var3_nd.txt -> var3.txt)

--- TAYAK_lab_02/Program.cs
+++ TAYAK_lab_02/Program.cs
@@ -9,11 +9,20 @@ namespace TAYAK_lab_02
     {
         static void Main(string[] args)
         {
+            string fileToOpen;
+            string baseAddr = @"C:\Users\admin\Source\Repos\TAYAK_lab2\TAYAK_lab_02\";
+            Console.WriteLine("Напишите, какой файл открыть (var1, var2, var3).");
+            fileToOpen = Console.ReadLine();
+            if (fileToOpen != "var1" && fileToOpen != "var2" && fileToOpen != "var3")
+            {
+                Console.WriteLine("Неизвестный файл. Закрываюсь");
+                Environment.Exit(0);
+            }
+
             try
             { 
-                using (StreamReader sr = new StreamReader("var1.txt"))
+                using (StreamReader sr = new StreamReader(baseAddr + fileToOpen + ".txt"))
                 {
-                    // Read the stream to a string, and write the string to the console.
                     String line = sr.ReadToEnd();
                     Console.WriteLine(line);
                 }
