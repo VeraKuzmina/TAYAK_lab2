583112e559dd7658cf46cb8ae954194a1da3b52f
Vera Kuzmina <vera-flashka@mail.ru>
2016-09-24 17:21:34 +0300

    Что-то похожее на детерминирование автомата

--- TAYAK_lab_02/Program.cs
+++ TAYAK_lab_02/Program.cs
@@ -5,65 +5,54 @@ using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 
-namespace TAYAK_lab_02
-{
-    class Program
-    {
-        private static string outputArray(List<Tuple<char, int>> list)
-        {
+namespace TAYAK_lab_02 {
+    class Program {
+        private static string outputArray(List<Tuple<char, int>> list) {
             string str = "";
             int i = 0;
-            foreach (var l in list)
-            {
+            foreach (var l in list) {
                 str = str + list[i].Item1 + Convert.ToString(list[i].Item2) + " ";
                 i++;
             }
             return str;
         }
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             string fileToOpen;
             string baseAddr = @"C:\Users\admin\Source\Repos\TAYAK_lab2\TAYAK_lab_02\";
-            Console.WriteLine("Напишите, какой файл открыть (var1, var2, var3).");
+
+            Console.Write("Напишите, какой файл открыть: ");
             fileToOpen = Console.ReadLine();
-            if (fileToOpen != "var1" && fileToOpen != "var2" && fileToOpen != "var3")
-            {
-                Console.WriteLine("Неизвестный файл. Закрываюсь");
-                Environment.Exit(0);
-            }
-            try
-            {
-                using (StreamReader sr = new StreamReader(baseAddr + fileToOpen + ".txt"))
-                {
+            try {
+                using (StreamReader sr = new StreamReader(baseAddr + fileToOpen + ".txt")) {
                     String line;
                     StateReader s = new StateReader();
-                    while ((line = sr.ReadLine()) != null)
-                    {
+                    while ((line = sr.ReadLine()) != null) {
                         Console.WriteLine(line);
                         s.addState(line);
                     }
                 }
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.WriteLine("Ошибка при открытии файла.");
                 Console.WriteLine(e.Message);
+                Environment.Exit(0);
             }
 
-            if (StateReader.isDetermine)
+            StateMachine sm = new StateMachine();
+            if (StateReader.isDetermine) {
                 Console.WriteLine("\t** Автомат недетерминирован. **");
-            else
-                Console.WriteLine("\t** Автомат детерминирован. **");
+                foreach (var key in StateReader.stateDic.Keys)
+                    Console.WriteLine("\tKey: {0} | Value: {1}", key, Program.outputArray(StateReader.stateDic[key]));
+                sm.determine();
+            }
 
+            Console.WriteLine("\t** Автомат детерминирован. **");
             foreach (var key in StateReader.stateDic.Keys)
                 Console.WriteLine("\tKey: {0} | Value: {1}", key, Program.outputArray(StateReader.stateDic[key]));
-
-            StateMachine sm = new StateMachine();
 
             Console.WriteLine("\nВведите строку символов для разбора:");
             string inCharacterString = Console.ReadLine();
             sm.analyzeString(inCharacterString);
-
         }
     }
 }
--- TAYAK_lab_02/StateMachine.cs
+++ TAYAK_lab_02/StateMachine.cs
@@ -4,26 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TAYAK_lab_02
-{
-    class StateMachine
-    {
+namespace TAYAK_lab_02 {
+    class StateMachine {
         private int stateNumber;
         private bool isStateTerminal, flag;
         private char stateCharacter;
 
-        public StateMachine()
-        {
+        public StateMachine() {
             stateNumber = 0;
             stateCharacter = 'q';
             isStateTerminal = false;
             flag = false;
         }
 
-        private void readCharacter(int i, string inString, char inChar)
-        {
-            try
-            {
+        private void readCharacter(int i, string inString, char inChar) {
+            try {
                 int new_stateNumber = StateReader.stateDic[Tuple.Create<char, int, char>(stateCharacter, stateNumber, inChar)][0].Item2;
                 char new_stateCharacter = StateReader.stateDic[Tuple.Create<char, int, char>(stateCharacter, stateNumber, inChar)][0].Item1;
                 if (i == inString.Length - 1)
@@ -32,15 +27,13 @@ namespace TAYAK_lab_02
                 stateNumber = new_stateNumber;
                 stateCharacter = new_stateCharacter;
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Console.WriteLine("Нет перехода.");
                 flag = true;
             }
         }
 
-        public void analyzeString(string inString)
-        {
+        public void analyzeString(string inString) {
             int i = 0;
             for (i = 0; i < inString.Length && !isStateTerminal; ++i)
                 readCharacter(i, inString, inString[i]);
@@ -50,6 +43,31 @@ namespace TAYAK_lab_02
                 Console.WriteLine("Автомат разобрал не всю строку.");
             else
                 Console.WriteLine("Автомат не сможет разобрать строку.");
+        }
+
+        public void determine()  {
+            var keys = StateReader.stateDic.Keys;
+            foreach (var key in keys) {
+                if (StateReader.stateDic[Tuple.Create<char, int, char>(key.Item1, key.Item2, key.Item3)].Count > 1) {
+                    List<Tuple<char, int>> list = StateReader.stateDic[key];
+                    int i = 0;
+                    string str = "";
+                    bool f = false;
+                    foreach (var l in list) {
+                        if (StateReader.stateDic[Tuple.Create<char, int, char>(key.Item1, key.Item2, key.Item3)][i].Item1 == 'f')
+                            f = true;
+                        str = str +  Convert.ToString(list[i].Item2);
+                        i++;
+                    }
+                    for (int j = 0; j<i; j++)
+                         list.RemoveAt(0);
+                    char inState;
+                    if (f) inState = 'F';
+                    else inState = 'Q';
+                    
+                    StateReader.stateDic[Tuple.Create<char, int, char>(key.Item1, key.Item2, key.Item3)].Add(new Tuple<char, int>(inState, int.Parse(str)));
+                }
+            }
         }
     }
 }
