4b637be92e37781b53c0365fb5990dc89f37ed14
Vera Kuzmina <vera-flashka@mail.ru>
2016-09-28 22:37:16 +0300

    Очень плохой, но правильно работающий код.

--- TAYAK_lab_02/StateReader.cs
+++ TAYAK_lab_02/StateReader.cs
@@ -9,20 +9,28 @@ namespace TAYAK_lab_02 {
     class StateReader {
         private bool flag, flag2;
         public static bool isDetermine;
+        public static bool hasEpsilon;
         public static Dictionary<Tuple<char, int, char>, List<Tuple<char, int>>> stateDic = new Dictionary<Tuple<char, int, char>, List<Tuple<char, int>>>();
 
         public StateReader() {
             flag = false;
             isDetermine = false;
+            hasEpsilon = false;
         }
         
         public void addState(string input) {
+
             List<Tuple<char, int>> list = new List<Tuple<char, int>>();
             int indexOfComma = input.IndexOf(',');
             char inChar = input[indexOfComma + 1];
             char inState = input[0];
             int curState = int.Parse(input.Substring(1, indexOfComma - 1));
             int  nextState = int.Parse(input.Substring(indexOfComma + 4));
+
+          //  if ((inState == input[indexOfComma + 3]) && (inChar == '~'))
+              //  return;
+
+            if (inChar == '~') hasEpsilon = true;
 
             if (!flag) {
                 list.Add(new Tuple<char, int>(input[indexOfComma + 3], nextState));
